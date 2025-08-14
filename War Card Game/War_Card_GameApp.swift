026055783cffb0530c2ef683079592fda3feda7e
Reut Farkash <reutfarkash@gmail.com>
2025-08-14 12:33:31 +0200

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Reut Farkash on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
